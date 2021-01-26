8f07577612603c370cfe81b49dcf3d43cd96290a
Dimitri <65912918+dimitri12482@users.noreply.github.com>
2021-01-26 21:42:33 +0000

    Update GithubButtons.jsx

--- src/components/GithubButtons/GithubButtons.jsx
+++ src/components/GithubButtons/GithubButtons.jsx
@@ -5,10 +5,24 @@ const GithubButton = () => (
   <>
     <GitHubButton
       className="github-button"
-      href="https://github.com/dimitri12482/Portfolioweb/"
+      href="https://github.com/dimitri12482/Portfolioweb/fork"
+      data-icon="octicon-repo-forked"
+      data-size="large"
+      data-show-count="true"
+      aria-label="Fork dimitri12482 on GitHub"
     >
-
+      Fork
     </GitHubButton>
+    <GitHubButton
+      className="github-button"
+      href="https://github.com/dimitri12482/Portfolioweb"
+      data-icon="octicon-star"
+      data-size="large"
+      data-show-count="true"
+      aria-label="Star dimitri12482 on GitHub"
+    >
+      Star
+    </GitHubButton>
+  </>
 );
-
 export default GithubButton;
